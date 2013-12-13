e39567de2f280214551633b0c610a2770b976c37
WingsOfLife <jkaffrey@comcast.net>
2013-12-13 10:36:48 -0700

    Committing Time
    Committing Time

--- inventorySystem/Client/GUI/ExtraInvOverlay.java
+++ inventorySystem/Client/GUI/ExtraInvOverlay.java
@@ -1,5 +1,7 @@
 package doc.inventorySystem.Client.GUI;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.util.ResourceLocation;
@@ -9,12 +11,20 @@ import net.minecraftforge.event.EventPriority;
 import net.minecraftforge.event.ForgeSubscribe;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import doc.inventorySystem.Common.ExtendedEntityRender;
 
 @SideOnly(Side.CLIENT)
 public class ExtraInvOverlay extends Gui {
 
 	private Minecraft mc;
-	public static final ResourceLocation extraInventory = new ResourceLocation("", "");
+	
+	int xPos = 2;
+	int yPos = 2;
+	
+	int textureXSize = 0;
+	int textureYSize = 0;
+	
+	public static final ResourceLocation extraInventory = new ResourceLocation("inventorySystems", "textures/gui/liquidTank.png");
 	
 	public ExtraInvOverlay(Minecraft mc) {
 		super();
@@ -27,6 +37,22 @@ public class ExtraInvOverlay extends Gui {
 			return;
 		}
 		
+		ExtendedEntityRender props = new ExtendedEntityRender(mc.thePlayer);
+		if (props == null)
+			return;
 		
+		int xCenter = (event.resolution.getScaledWidth() + textureXSize) / 2;
+		int yCenter = (event.resolution.getScaledHeight() + textureYSize) / 2;
+		
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL11.glDepthMask(false);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		GL11.glDisable(GL11.GL_ALPHA_TEST);
+		mc.renderEngine.func_110577_a(extraInventory); //TODO: Bind Texture updating ForgeVersion
+		drawTexturedModalRect(xCenter, yCenter, 0, 0, textureXSize, textureYSize);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glDepthMask(true);
 	}
 }
--- inventorySystem/InventorySystemCore.java
+++ inventorySystem/InventorySystemCore.java
@@ -1,13 +1,17 @@
 package doc.inventorySystem;
 
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import doc.inventorySystem.Client.GUI.ExtraInvOverlay;
 import doc.inventorySystem.Common.CommonProxy;
 import doc.inventorySystem.Common.ISEventHandler;
 import doc.inventorySystem.Packets.PacketHandler;
@@ -30,6 +34,12 @@ public class InventorySystemCore {
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(new ISEventHandler());
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		if (FMLCommonHandler.instance().getEffectiveSide().isClient())
+			MinecraftForge.EVENT_BUS.register(new ExtraInvOverlay(Minecraft.getMinecraft()));
 	}
 	
 }
